08948ce79426c82293043e108b98bf29e490cc2a
zhangshaoqi <1131558536@qq.com>
2017-01-04 15:25:01 +0800

    本地模式和集群模式

--- project-storm/src/main/java/com/phantom/storm/practice/wordcount/WordCountTopology.java
+++ project-storm/src/main/java/com/phantom/storm/practice/wordcount/WordCountTopology.java
@@ -2,6 +2,7 @@ package com.phantom.storm.practice.wordcount;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
 
@@ -21,16 +22,21 @@ public class WordCountTopology {
 		ReportBolt reportBolt = new ReportBolt();
 		
 		TopologyBuilder builder = new TopologyBuilder();
-		builder.setSpout(SENTENCE_SPOUT_ID, spout);
-		builder.setBolt(SPLIT_BOLT_ID, splitBolt).shuffleGrouping(SENTENCE_SPOUT_ID);
-		builder.setBolt(COUNT_BOLT_ID, countBolt).fieldsGrouping(SPLIT_BOLT_ID,new Fields("word"));
+		builder.setSpout(SENTENCE_SPOUT_ID, spout, 2);
+		builder.setBolt(SPLIT_BOLT_ID, splitBolt, 2).setNumTasks(4).shuffleGrouping(SENTENCE_SPOUT_ID);
+		builder.setBolt(COUNT_BOLT_ID, countBolt, 4).fieldsGrouping(SPLIT_BOLT_ID,new Fields("word"));
 		builder.setBolt(REPORT_BOLT_ID, reportBolt).globalGrouping(COUNT_BOLT_ID);
 				
 		Config config = new Config();
-		LocalCluster cluster = new LocalCluster();
-		cluster.submitTopology(TOPOLOGY_NAME, config, builder.createTopology());
-		Thread.sleep(10000);
-		cluster.killTopology(TOPOLOGY_NAME);
-		cluster.shutdown();
+		config.setNumWorkers(5);
+		if(args.length==0){
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology(TOPOLOGY_NAME, config, builder.createTopology());
+			Thread.sleep(10000);
+			cluster.killTopology(TOPOLOGY_NAME);
+			cluster.shutdown();
+		}else{
+			StormSubmitter.submitTopology(args[0], config, builder.createTopology());
+		}
 	}
 }
