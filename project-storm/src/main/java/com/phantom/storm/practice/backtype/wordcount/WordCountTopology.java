230134e5f3b9c08b57d8f7063aa3dd2d5613c160
zhangshaoqi <1131558536@qq.com>
2017-01-10 18:20:49 +0800

    完善topology

--- project-storm/src/main/java/com/phantom/storm/practice/backtype/wordcount/WordCountTopology.java
+++ project-storm/src/main/java/com/phantom/storm/practice/backtype/wordcount/WordCountTopology.java
@@ -3,6 +3,7 @@ package com.phantom.storm.practice.backtype.wordcount;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
+import backtype.storm.generated.StormTopology;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
 
@@ -13,9 +14,9 @@ public class WordCountTopology {
 	private static final String COUNT_BOLT_ID = "count-bolt";
 	private static final String REPORT_BOLT_ID = "report-bolt";
 	private static final String TOPOLOGY_NAME = "word-count-topology";
+	
+	public static StormTopology buildTopology() {
 
-	public static void main(String[] args) throws Exception{
-		
 		SentenceSpout spout = new SentenceSpout();
 		SplitSentenceBolt splitBolt = new SplitSentenceBolt();
 		WordCountBolt countBolt = new WordCountBolt();
@@ -26,17 +27,22 @@ public class WordCountTopology {
 		builder.setBolt(SPLIT_BOLT_ID, splitBolt, 2).setNumTasks(4).shuffleGrouping(SENTENCE_SPOUT_ID);
 		builder.setBolt(COUNT_BOLT_ID, countBolt, 4).fieldsGrouping(SPLIT_BOLT_ID,new Fields("word"));
 		builder.setBolt(REPORT_BOLT_ID, reportBolt).globalGrouping(COUNT_BOLT_ID);
-				
-		Config config = new Config();
-		config.setNumWorkers(5);
+		
+		return builder.createTopology();
+	}
+
+	public static void main(String[] args) throws Exception{
+		
+		Config conf = new Config();
+		conf.setNumWorkers(5);
 		if(args.length==0){
 			LocalCluster cluster = new LocalCluster();
-			cluster.submitTopology(TOPOLOGY_NAME, config, builder.createTopology());
+			cluster.submitTopology(TOPOLOGY_NAME, conf, buildTopology());
 			Thread.sleep(10000);
 			cluster.killTopology(TOPOLOGY_NAME);
 			cluster.shutdown();
 		}else{
-			StormSubmitter.submitTopology(args[0], config, builder.createTopology());
+			StormSubmitter.submitTopology(args[0], conf, buildTopology());
 		}
 	}
 }
