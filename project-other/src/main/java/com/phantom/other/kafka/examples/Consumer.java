78dff7aad950a4e0d787afc7dfd15f58669381f7
phantom <phantom@phantom-PC>
2017-01-22 00:37:44 +0800

    多线程接受topic的每个partition

--- project-other/src/main/java/com/phantom/other/kafka/examples/Consumer.java
+++ project-other/src/main/java/com/phantom/other/kafka/examples/Consumer.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import kafka.consumer.ConsumerConfig;
 import kafka.consumer.ConsumerIterator;
@@ -23,9 +25,10 @@ public class Consumer extends Thread {
 		props.put("zookeeper.session.timeout.ms", "400");
 		props.put("zookeeper.sync.time.ms", "200");
 		props.put("auto.commit.interval.ms", "1000000");
-		consumer = kafka.consumer.Consumer.createJavaConsumerConnector(new ConsumerConfig(props));
+		consumer = kafka.consumer.Consumer
+				.createJavaConsumerConnector(new ConsumerConfig(props));
 	}
-	
+
 	public Consumer(String topic) {
 
 		this();
@@ -36,11 +39,19 @@ public class Consumer extends Thread {
 	public void run() {
 		
 		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-		topicCountMap.put(topic, new Integer(1));// 后面数字还应该是开几个线程去读取消息
+		topicCountMap.put(topic, new Integer(2));// 后面数字还应该是开几个线程去读取消息
 		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-		KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);
-		ConsumerIterator<byte[], byte[]> it = stream.iterator();
-		while (it.hasNext())
-			System.out.println(new String(it.next().message()));
+		List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
+		ExecutorService service = Executors.newFixedThreadPool(4);
+		for(final KafkaStream<byte[], byte[]> stream : streams){
+			service.submit(new Runnable() {
+				@Override
+				public void run() {
+					ConsumerIterator<byte[], byte[]> it = stream.iterator();
+					while (it.hasNext())
+						System.out.println(new String(it.next().message()));
+				}
+			});
+		}
 	}
 }
