a4197387bae3c9ae801db94915ef5ebf96351468
zhangshaoqi <1131558536@qq.com>
2017-03-24 11:06:41 +0800

    修改消费端简单api

--- project-storm/src/main/java/com/phantom/kafka/examples/SimpleConsumerDemo.java
+++ project-storm/src/main/java/com/phantom/kafka/examples/SimpleConsumerDemo.java
@@ -32,30 +32,13 @@ public class SimpleConsumerDemo {
 		}
 	}
 
-	private static void generateData() {
-
-		Producer producer2 = new Producer(topic2);
-		producer2.start();
-		Producer producer3 = new Producer(topic3);
-		producer3.start();
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
 	public static void main(String[] args) throws Exception {
 
-		generateData();
-
 		SimpleConsumer simpleConsumer = new SimpleConsumer("slave", 9092, 100000, 64 * 1024, clientId);
-
 		System.out.println("Testing single fetch");
 		FetchRequest req = new FetchRequestBuilder().clientId(clientId).addFetch(topic2, 0, 0L, 100).build();
 		FetchResponse fetchResponse = simpleConsumer.fetch(req);
-		printMessages((ByteBufferMessageSet) fetchResponse.messageSet(topic2, 0));
-
+		printMessages(fetchResponse.messageSet(topic2, 0));
 		System.out.println("Testing single multi-fetch");
 		Map<String, List<Integer>> topicMap = new HashMap<String, List<Integer>>() {
 			{
@@ -71,15 +54,14 @@ public class SimpleConsumerDemo {
 				});
 			}
 		};
-		req = new FetchRequestBuilder().clientId(clientId).addFetch(topic2, 0, 0L, 100).addFetch(topic3, 0, 0L, 100)
-				.build();
+		req = new FetchRequestBuilder().clientId(clientId).addFetch(topic2, 0, 0L, 100).addFetch(topic3, 0, 0L, 100).build();
 		fetchResponse = simpleConsumer.fetch(req);
 		int fetchReq = 0;
 		for (Map.Entry<String, List<Integer>> entry : topicMap.entrySet()) {
 			String topic = entry.getKey();
 			for (Integer offset : entry.getValue()) {
 				System.out.println("Response from fetch request no: " + ++fetchReq);
-				printMessages((ByteBufferMessageSet) fetchResponse.messageSet(topic, offset));
+				printMessages(fetchResponse.messageSet(topic, offset));
 			}
 		}
 	}
